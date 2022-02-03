781c0104f78919cc71075b33f63ba713fd7e5d19
smo92 <moran.s215@gmail.com>
2022-02-03 13:19:52 -0700

    trouble shooting code

--- js/script.js
+++ js/script.js
@@ -2,33 +2,30 @@ function newItem(){
     let li = $('<li></li>');
     let inputValue = $('input').val();
 
-    li.append(document.createTextNode(inputValue));
+    li.append(inputValue);
 
     //Adding item to list with Add button
-    if (inputValue = ''){
+    if (inputValue === ''){
         alert('This feild is required');
     }else{
         $('#list').append(li);
     }
     
     //put line through items when clicked
-    function crossOut(){
-        li.toggleClass('strike');
-    }
-        li.on('dbclick', function crossOut(){
-            li.toggleClass('strike');
-        });
+    li.on('dbclick', function(){
+     li.addClass('strike');
+   });
 
     //adding delete button
-    let deleteButton = $(<button>X</button>);
-    deleteButton.addClass('crossOutButton');
-    li.append(deleteButton);
+    let crossOutButton = $('<crossOutButton></crossOutButton>');
+      crossOutButton.append(document.createTextNode('X'));
+      li.append(crossOutButton);
+      crossOutButton.on('click', deleteItem);
 
     //delete list item when button is clicked
     function deleteItem(){
         li.addClass('delete');
     }
-    deleteButton.on('click', deleteItem);
 
     //change order of list
     $('#list').sortable();// list is sortable using mouse
